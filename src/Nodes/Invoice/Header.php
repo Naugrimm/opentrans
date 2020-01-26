3fdc582947d225ce9e10f6f8bc04baee868ba295
Erik <erik.gto@googlemail.com>
2020-01-26 15:09:19 +0100

    feat: implement ORDER_HISTORY

--- src/Nodes/Invoice/Header.php
+++ src/Nodes/Invoice/Header.php
@@ -6,11 +6,12 @@ use /** @noinspection PhpUnusedAliasInspection */
     JMS\Serializer\Annotation as Serializer;
 use Naugrim\OpenTrans\Nodes\Concerns\HasControlInfo;
 use Naugrim\OpenTrans\Nodes\NodeInterface;
+use Naugrim\OpenTrans\Nodes\Order\History;
 
 class Header implements NodeInterface
 {
     use HasControlInfo;
-    
+
     /**
      * @Serializer\Expose
      * @Serializer\Type("Naugrim\OpenTrans\Nodes\Invoice\Info")
@@ -19,6 +20,15 @@ class Header implements NodeInterface
      * @var Info
      */
     protected $info;
+
+    /**
+     * @Serializer\Expose
+     * @Serializer\Type("Naugrim\OpenTrans\Nodes\Order\History")
+     * @Serializer\SerializedName("ORDER_HISTORY")
+     *
+     * @var History
+     */
+    protected $orderHistory;
 
     /**
      * @return Info
@@ -35,6 +45,24 @@ class Header implements NodeInterface
     public function setInfo(Info $info): Header
     {
         $this->info = $info;
+        return $this;
+    }
+
+    /**
+     * @return History
+     */
+    public function getOrderHistory(): History
+    {
+        return $this->orderHistory;
+    }
+
+    /**
+     * @param History $orderHistory
+     * @return Header
+     */
+    public function setOrderHistory(History $orderHistory): Header
+    {
+        $this->orderHistory = $orderHistory;
         return $this;
     }
 }
