c4474f2dc441438072499d325c51216b3e3e310e
Benjamin Josefus <benjamin.josefus@hmmh.de>
2022-03-11 14:11:08 +0100

    fix datetime fixture in unit tests

--- tests/Nodes/OrderChangeTest.php
+++ tests/Nodes/OrderChangeTest.php
@@ -28,7 +28,7 @@ class OrderChangeTest extends TestCase
             'header' => [
                 'info' => [
                     'id' => 'order-id-1',
-                    'date' => (new DateTimeImmutable())->format('Y-m-d'),
+                    'date' => (new DateTimeImmutable('2020-01-27'))->format('Y-m-d'),
                     'sequenceId' => 1,
                     'parties' => [
                         [
