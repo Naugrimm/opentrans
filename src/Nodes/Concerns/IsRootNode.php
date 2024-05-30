6c2220c15e759e56736c0b22a1794b9b29cd59e4
Erik Anders <5753604+Naugrimm@users.noreply.github.com>
2024-05-30 16:27:34 +0200

    fix: register the XmlNamespace attributes correctly
    issueid #14 @0m

--- src/Nodes/Concerns/IsRootNode.php
+++ src/Nodes/Concerns/IsRootNode.php
@@ -8,16 +8,6 @@ use Naugrim\OpenTrans\OpenTrans;
 trait IsRootNode
 {
     #[Serializer\Expose]
-    #[Serializer\SerializedName('xmlns:bme')]
-    #[Serializer\XmlAttribute]
-    protected string $bmecatNamespace = OpenTrans::BMECAT_NAMESPACE;
-
-    #[Serializer\Expose]
-    #[Serializer\SerializedName('xmlns')]
-    #[Serializer\XmlAttribute]
-    protected string $opentransNamespace = OpenTrans::OPENTRANS_NAMESPACE;
-
-    #[Serializer\Expose]
     #[Serializer\XmlAttribute]
     protected string $version = OpenTrans::OPENTRANS_VERSION;
 }
