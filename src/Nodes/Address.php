423d38b27fea7bbf2ddd6c5db82c2fda820a001f
Erik Anders <5753604+Naugrimm@users.noreply.github.com>
2024-06-04 09:32:49 +0200

    feat: make address properties nullable as per opentrans spec
    refs #0 @0m

--- src/Nodes/Address.php
+++ src/Nodes/Address.php
@@ -12,36 +12,36 @@ use Naugrim\OpenTrans\Nodes\Contact\Details;
 
 /**
  * @implements NodeInterface<Address>
- * @method self setName(string $name)
- * @method string getName()
- * @method self setName2(string $name2)
- * @method string getName2()
- * @method self setName3(string $name3)
- * @method string getName3()
- * @method self setDepartment(string $department)
- * @method string getDepartment()
+ * @method self setName(string|null $name)
+ * @method string|null getName()
+ * @method self setName2(string|null $name2)
+ * @method string|null getName2()
+ * @method self setName3(string|null $name3)
+ * @method string|null getName3()
+ * @method self setDepartment(string|null $department)
+ * @method string|null getDepartment()
  * @method self setContactDetails(\Naugrim\OpenTrans\Nodes\Contact\Details[]|array $contactDetails)
  * @method \Naugrim\OpenTrans\Nodes\Contact\Details[]|array getContactDetails()
- * @method self setStreet(string $street)
- * @method string getStreet()
- * @method self setZip(string $zip)
- * @method string getZip()
- * @method self setBoxno(string $boxno)
- * @method string getBoxno()
- * @method self setZipbox(string $zipbox)
- * @method string getZipbox()
- * @method self setCity(string $city)
- * @method string getCity()
- * @method self setState(string $state)
- * @method string getState()
- * @method self setCountry(string $country)
- * @method string getCountry()
- * @method self setCountryCoded(string $countryCoded)
- * @method string getCountryCoded()
- * @method self setVatId(string $vatId)
- * @method string getVatId()
- * @method self setTaxNumber(string $taxNumber)
- * @method string getTaxNumber()
+ * @method self setStreet(string|null $street)
+ * @method string|null getStreet()
+ * @method self setZip(string|null $zip)
+ * @method string|null getZip()
+ * @method self setBoxno(string|null $boxno)
+ * @method string|null getBoxno()
+ * @method self setZipbox(string|null $zipbox)
+ * @method string|null getZipbox()
+ * @method self setCity(string|null $city)
+ * @method string|null getCity()
+ * @method self setState(string|null $state)
+ * @method string|null getState()
+ * @method self setCountry(string|null $country)
+ * @method string|null getCountry()
+ * @method self setCountryCoded(string|null $countryCoded)
+ * @method string|null getCountryCoded()
+ * @method self setVatId(string|null $vatId)
+ * @method string|null getVatId()
+ * @method self setTaxNumber(string|null $taxNumber)
+ * @method string|null getTaxNumber()
  * @method self setPhone(\Naugrim\BMEcat\Nodes\Phone[]|array $phone)
  * @method \Naugrim\BMEcat\Nodes\Phone[]|array getPhone()
  * @method self setFax(\Naugrim\BMEcat\Nodes\Fax[]|array $fax)
@@ -50,10 +50,10 @@ use Naugrim\OpenTrans\Nodes\Contact\Details;
  * @method string getEmails()
  * @method self setPublicKey(\Naugrim\BMEcat\Nodes\Crypto\PublicKey[]|array $publicKey)
  * @method \Naugrim\BMEcat\Nodes\Crypto\PublicKey[]|array getPublicKey()
- * @method self setUrl(string $url)
- * @method string getUrl()
- * @method self setAddressRemarks(string $addressRemarks)
- * @method string getAddressRemarks()
+ * @method self setUrl(string|null $url)
+ * @method string|null getUrl()
+ * @method self setAddressRemarks(string|null $addressRemarks)
+ * @method string|null getAddressRemarks()
  */
 class Address implements NodeInterface
 {
@@ -63,25 +63,25 @@ class Address implements NodeInterface
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('NAME')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $name;
+    protected ?string $name = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('NAME2')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $name2;
+    protected ?string $name2 = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('NAME3')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $name3;
+    protected ?string $name3 = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('DEPARTMENT')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $department;
+    protected ?string $department = null;
 
     /**
      * @var Details[]
@@ -95,60 +95,60 @@ class Address implements NodeInterface
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('STREET')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $street;
+    protected ?string $street = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('ZIP')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $zip;
+    protected ?string $zip = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('BOXNO')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $boxno;
+    protected ?string $boxno = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('ZIPBOX')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $zipbox;
+    protected ?string $zipbox = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('CITY')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $city;
+    protected ?string $city = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('STATE')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $state;
+    protected ?string $state = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('COUNTRY')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $country;
+    protected ?string $country = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('COUNTRY_CODED')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $countryCoded;
+    protected ?string $countryCoded = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('VAT_ID')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $vatId;
+    protected ?string $vatId = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('TAX_NUMBER')]
-    protected string $taxNumber;
+    protected ?string $taxNumber = null;
 
     /**
      * @var Phone[]
@@ -186,11 +186,11 @@ class Address implements NodeInterface
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('URL')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $url;
+    protected ?string $url = null;
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
     #[Serializer\SerializedName('ADDRESS_REMARKS')]
     #[\JMS\Serializer\Annotation\XmlElement(namespace: \Naugrim\OpenTrans\OpenTrans::BMECAT_NAMESPACE)]
-    protected string $addressRemarks;
+    protected ?string $addressRemarks = null;
 }
