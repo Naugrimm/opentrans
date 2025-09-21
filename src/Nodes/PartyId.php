f305dc106a8d3929d3939c9d97a763b7423b234d
Erik Anders <5753604+Naugrimm@users.noreply.github.com>
2025-09-21 19:41:17 +0200

    fix: add missing constant type hints
    refs #0 @0m

--- src/Nodes/PartyId.php
+++ src/Nodes/PartyId.php
@@ -25,23 +25,23 @@ class PartyId implements NodeInterface
     use HasTypeAttribute;
     use HasStringValue;
 
-    public const TYPE_SUPPLIER_SPECIFIC = 'supplier_specific';
+    public const string TYPE_SUPPLIER_SPECIFIC = 'supplier_specific';
 
-    public const TYPE_BUYER_SPECIFIC = 'buyer_specific';
+    public const string TYPE_BUYER_SPECIFIC = 'buyer_specific';
 
-    public const TYPE_CUSTOMER_SPECIFIC = 'customer_specific';
+    public const string TYPE_CUSTOMER_SPECIFIC = 'customer_specific';
 
-    public const TYPE_PARTY_SPECIFIC = 'party_specific';
+    public const string TYPE_PARTY_SPECIFIC = 'party_specific';
 
-    public const TYPE_DUNS = 'duns';
+    public const string TYPE_DUNS = 'duns';
 
-    public const TYPE_ILN = 'iln';
+    public const string TYPE_ILN = 'iln';
 
-    public const TYPE_GLN = 'gln';
+    public const string TYPE_GLN = 'gln';
 
-    public const TYPE_BUYER = 'buyer';
+    public const string TYPE_BUYER = 'buyer';
 
-    public const TYPE_ISSUER = 'issuer';
+    public const string TYPE_ISSUER = 'issuer';
 
-    public const TYPE_SUPPLIER = 'supplier';
+    public const string TYPE_SUPPLIER = 'supplier';
 }
--- src/Nodes/Payment/Card.php
+++ src/Nodes/Payment/Card.php
@@ -34,23 +34,23 @@ class Card implements NodeInterface
      */
     use HasTypeAttribute;
 
-    public const MASTER_CARD = 'MasterCard';
+    public const string MASTER_CARD = 'MasterCard';
 
-    public const VISA = 'VISA';
+    public const string VISA = 'VISA';
 
-    public const AMERICAN_EXPRESS = 'AmericanExpress';
+    public const string AMERICAN_EXPRESS = 'AmericanExpress';
 
-    public const JCB = 'JCB';
+    public const string JCB = 'JCB';
 
-    public const MAESTRO = 'Maestro';
+    public const string MAESTRO = 'Maestro';
 
-    public const DISCOVER_CARD = 'DiscoverCard';
+    public const string DISCOVER_CARD = 'DiscoverCard';
 
-    public const TRANS_CARD = 'Transcard';
+    public const string TRANS_CARD = 'Transcard';
 
-    public const DINA_CARD = 'DinaCard';
+    public const string DINA_CARD = 'DinaCard';
 
-    public const CHINA_UNION_PAY = 'ChinaUnionPay';
+    public const string CHINA_UNION_PAY = 'ChinaUnionPay';
 
     #[Serializer\Expose]
     #[Serializer\Type('string')]
