5978bef85b2bb27909ea3a5ffb3a3d59b5980003
Erik Anders <5753604+Naugrimm@users.noreply.github.com>
2025-09-23 16:56:26 +0200

    feat: implement SHIPMENT_PARTIES_REFERENCE in the ORDERRESPONSE_ITEM
    refs #0 @0m

--- tests/Nodes/OrderResponseTest.php
+++ tests/Nodes/OrderResponseTest.php
@@ -7,9 +7,14 @@ use JMS\Serializer\Serializer;
 use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\SerializerInterface;
 use Naugrim\BMEcat\Builder\NodeBuilder;
+use Naugrim\BMEcat\Exception\InvalidSetterException;
+use Naugrim\BMEcat\Exception\SchemaValidationException;
+use Naugrim\BMEcat\Exception\UnknownKeyException;
 use Naugrim\OpenTrans\Nodes\OrderResponse;
 use Naugrim\OpenTrans\SchemaValidator;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 class OrderResponseTest extends TestCase
 {
@@ -20,60 +25,151 @@ class OrderResponseTest extends TestCase
         $this->serializer = SerializerBuilder::create()->build();
     }
 
-    public function testMinimalOrderResponse(): void
+    /**
+     * @param string $file
+     * @param array<string, mixed> $data
+     * @throws InvalidSetterException
+     * @throws SchemaValidationException
+     * @throws UnknownKeyException
+     */
+    #[DataProvider('provideOrderResponseData')]
+    public function testOrderResponse(string $file, array $data): void
     {
-        $node = NodeBuilder::fromArray([
-            'header' => [
-                'info' => [
-                    'id' => 'order-id-1',
-                    'orderResponseDate' => new DateTimeImmutable('2020-01-27')->format('Y-m-d'),
-                    'sequenceId' => 1,
-                    'parties' => [
-                        [
-                            'id' => [
-                                'value' => 'org.de.supplier',
-                                'type' => 'supplier',
-                            ],
-                        ],
-                        [
-                            'id' => [
-                                'value' => 'org.de.buyer',
-                                'type' => 'buyer',
-                            ],
-                        ],
-                    ],
-                    'partiesReference' => [
-                        'buyerIdRef' => [
-                            'value' => 'org.de.buyer',
-                        ],
-                        'supplierIdRef' => [
-                            'value' => 'org.de.buyer',
-                        ],
-                    ],
-                ],
-            ],
-            'items' => [
-                [
-                    'lineItemId' => 'line-item-id-1',
-                    'productId' => [
-                        'supplierPid' => [
-                            'value' => 'product-number-1',
-                        ],
-                    ],
-                    'quantity' => 5,
-                    'orderUnit' => 'C62',
-                ],
-            ],
-            'summary' => [
-                'totalItemNum' => 1,
-            ],
-        ], NodeBuilder::fromArray([], OrderResponse::class));
+        $node = NodeBuilder::fromArray($data, NodeBuilder::fromArray([], OrderResponse::class));
+        try {
+            $xml = $this->serializer->serialize($node, 'xml');
+        } catch (Throwable $throwable) {
+            $this->fail($throwable->getMessage());
+        }
 
-
-        $xml = $this->serializer->serialize($node, 'xml');
-
-        $this->assertEquals(file_get_contents(__DIR__ . '/../assets/minimal_valid_orderresponse.xml'), $xml);
-
+        $this->assertEquals(file_get_contents($file), $xml);
         $this->assertTrue(SchemaValidator::isValid($xml, '2.1'));
+    }
+
+    /**
+     * @return array<string, mixed>[]
+     */
+    public static function provideOrderResponseData(): array
+    {
+        return [
+            [
+                'file' => __DIR__ . '/../assets/minimal_valid_orderresponse.xml',
+                'data' => [
+                    'header' => [
+                        'info' => [
+                            'id' => 'order-id-1',
+                            'orderResponseDate' => (new DateTimeImmutable('2020-01-27'))->format('Y-m-d'),
+                            'sequenceId' => 1,
+                            'parties' => [
+                                [
+                                    'id' => [
+                                        'value' => 'org.de.supplier',
+                                        'type' => 'supplier',
+                                    ],
+                                ],
+                                [
+                                    'id' => [
+                                        'value' => 'org.de.buyer',
+                                        'type' => 'buyer',
+                                    ],
+                                ],
+                            ],
+                            'partiesReference' => [
+                                'buyerIdRef' => [
+                                    'value' => 'org.de.buyer',
+                                ],
+                                'supplierIdRef' => [
+                                    'value' => 'org.de.buyer',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'items' => [
+                        [
+                            'lineItemId' => 'line-item-id-1',
+                            'productId' => [
+                                'supplierPid' => [
+                                    'value' => 'product-number-1',
+                                ],
+                            ],
+                            'quantity' => 5,
+                            'orderUnit' => 'C62',
+                        ],
+                    ],
+                    'summary' => [
+                        'totalItemNum' => 1,
+                    ],
+                ],
+            ],
+            [
+                'file' => __DIR__ . '/../assets/minimal_valid_orderresponse_with_shipment_parties_reference.xml',
+                'data' => [
+                    'header' => [
+                        'info' => [
+                            'id' => 'order-id-1',
+                            'orderResponseDate' => (new DateTimeImmutable('2020-01-27'))->format('Y-m-d'),
+                            'sequenceId' => 1,
+                            'parties' => [
+                                [
+                                    'id' => [
+                                        'value' => 'org.de.supplier',
+                                        'type' => 'supplier',
+                                    ],
+                                ],
+                                [
+                                    'id' => [
+                                        'value' => 'org.de.buyer',
+                                        'type' => 'buyer',
+                                    ],
+                                ],
+                                [
+                                    'id' => [
+                                        'value' => 'org.de.delivery',
+                                    ],
+                                ],
+                            ],
+                            'partiesReference' => [
+                                'buyerIdRef' => [
+                                    'value' => 'org.de.buyer',
+                                ],
+                                'supplierIdRef' => [
+                                    'value' => 'org.de.buyer',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'items' => [
+                        [
+                            'lineItemId' => 'line-item-id-1',
+                            'productId' => [
+                                'supplierPid' => [
+                                    'value' => 'product-number-1',
+                                ],
+                            ],
+                            'quantity' => 5,
+                            'orderUnit' => 'C62',
+                            'priceFix' => [
+                                'amount' => 100,
+                            ],
+                            'priceLineAmount' => 500,
+                            'shipmentPartiesReference' => [
+                                'deliveryIdRef' => [
+                                    'value' => 'org.de.delivery',
+                                ],
+                                'finalDeliveryIdRef' => [
+                                    'value' => 'org.de.final.delivery',
+                                ],
+                                'delivererIdRef' => [
+                                    'value' => 'org.de.deliverer',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'summary' => [
+                        'totalItemNum' => 1,
+                    ],
+                ],
+            ],
+        ];
     }
 }
